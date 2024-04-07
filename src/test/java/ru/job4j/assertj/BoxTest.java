ebad45aef73dce001a8741daf5a6d08683045765
rkouzmine <krrsea@gmail.com>
2024-04-07 18:02:47 +0300

    Утверждения с примитивными типами

---
+++ src/test/java/ru/job4j/assertj/BoxTest.java
@@ -0,0 +1,82 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class BoxTest {
+
+    @Test
+    void isThisSphere() {
+        Box box = new Box(0, 10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Sphere");
+    }
+
+    @Test
+    void isThisTetrahedron() {
+        Box box = new Box(4, 4);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Tetrahedron");
+    }
+
+    @Test
+    void isThisCube() {
+        Box box = new Box(8, 4);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Cube");
+    }
+
+    @Test
+    void isThisUNKNOWN() {
+        Box box = new Box(6, 0);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Unknown object");
+    }
+
+    @Test
+    void isThisPositive() {
+        Box box = new Box(8, 5);
+        int result = box.getNumberOfVertices();
+        assertThat(result)
+                .isPositive()
+                .isLessThan(10)
+                .isEven()
+                .isEqualTo(8);
+    }
+
+    @Test
+    void isThisNegative() {
+        Box box = new Box(5, 8);
+        int result = box.getNumberOfVertices();
+        assertThat(result).isEqualTo(-1);
+    }
+
+    @Test
+    void whenExistIsTrue() {
+        Box box = new Box(0, 5);
+        boolean result = box.isExist();
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void whenExistIsFalse() {
+        Box box = new Box(4, 0);
+        boolean result = box.isExist();
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void whenAreaIsTrue() {
+        Box box = new Box(4, 5);
+        double result = box.getArea();
+        assertThat(result).isEqualTo(43.3d, withPrecision(0.01d));
+    }
+
+    @Test
+    void whenAreaIsFalse() {
+        Box box = new Box(6, 6);
+        double result = box.getArea();
+        assertThat(result).isEqualTo(0.0);
+    }
+}
