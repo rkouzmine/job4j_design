8c2bd8ce32220123c52291c13d203fd6927c9965
rkouzmine <krrsea@gmail.com>
2024-05-01 02:34:33 +0300

    Переопределить и equals и hashCode

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -2,6 +2,7 @@ package ru.job4j.map;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class User {
     private final String name;
@@ -15,6 +16,23 @@ public class User {
         this.birthday = birthday;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
     public static void main(String[] args) {
         Calendar birthday = new Calendar();
         User userFirst = new User("user", 2, birthday);
@@ -22,6 +40,7 @@ public class User {
         Map<User, Object> map = new HashMap<>();
         map.put(userFirst, new Object());
         map.put(userSecond, new Object());
+        System.out.println(map);
         int hashCodeFirst = userFirst.hashCode();
         int hashFirst = hashCodeFirst ^ (hashCodeFirst >>> 16);
         int bucketFirst = hashFirst & 15;
