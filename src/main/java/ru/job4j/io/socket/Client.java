804c08561d1ccf5376223fab16c5ead97a6a5c2e
rkouzmine <krrsea@gmail.com>
2024-08-27 00:53:44 +0300

    Java Client Server Socket

---
+++ src/main/java/ru/job4j/io/socket/Client.java
@@ -0,0 +1,27 @@
+package ru.job4j.io.socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        Socket clientSocket = new Socket("127.0.0.1", 8000);
+
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+        OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
+
+        writer.write("Get me some information\n");
+        writer.flush();
+
+        String msg = reader.readLine();
+        System.out.println(msg);
+
+        writer.close();
+        reader.close();
+        clientSocket.close();
+    }
+}
---
+++ src/main/java/ru/job4j/io/socket/Server.java
@@ -0,0 +1,31 @@
+package ru.job4j.io.socket;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        int count = 0;
+        ServerSocket serverSocket = new ServerSocket(8000);
+
+        while (true) {
+            System.out.println("Server's up!");
+            Socket clientSocket = serverSocket.accept();
+
+            System.out.println("Client accepted" + ++count + "\n");
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
+
+            String request = reader.readLine();
+            String response = "Client #" + count + ", your message length is: " + request.length() + "\n";
+            writer.write(response);
+            writer.flush();
+
+            reader.close();
+            writer.close();
+            clientSocket.close();
+        }
+    }
+}
