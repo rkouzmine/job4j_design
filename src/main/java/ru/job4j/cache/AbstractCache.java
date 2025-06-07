efc6caf765acb9b44fa82ab27235f6cd802dc3c8
rkouzmine <krrsea@gmail.com>
2025-06-08 01:28:44 +0300

    Реализация кеша на SoftReference

--- src/main/java/ru/job4j/cache/AbstractCache.java
+++ src/main/java/ru/job4j/cache/AbstractCache.java
@@ -9,11 +9,19 @@ public abstract class AbstractCache<K, V> {
     private final Map<K, SoftReference<V>> cache = new HashMap<>();
 
     public final void put(K key, V value) {
-
+        cache.put(key, new SoftReference<>(value));
     }
 
     public final V get(K key) {
-        return null;
+        SoftReference<V> softReference = cache.get(key);
+        V value;
+        if (softReference != null) {
+            value = softReference.get();
+        } else {
+            value = load(key);
+            put(key, value);
+        }
+        return value;
     }
 
     protected abstract V load(K key);
--- src/main/java/ru/job4j/cache/DirFileCache.java
+++ src/main/java/ru/job4j/cache/DirFileCache.java
@@ -1,8 +1,8 @@
 package ru.job4j.cache;
 
+import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.io.IOException;
+import java.nio.file.Path;
 
 public class DirFileCache extends AbstractCache<String, String> {
 
@@ -14,6 +14,35 @@ public class DirFileCache extends AbstractCache<String, String> {
 
     @Override
     protected String load(String key) {
-        return null;
+        Path path = Path.of(cachingDir, key);
+        String result = null;
+        if (Files.exists(path)) {
+            if (Files.isRegularFile(path)) {
+                if (Files.isReadable(path)) {
+                    try {
+                        result = Files.readString(path);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else {
+                    System.out.println(String.format(
+                            "Недостаточно прав для чтения файла '%s'. Проверьте права доступа к файлу.",
+                            key
+                    ));
+                }
+            } else {
+                System.out.println(String.format(
+                        "Путь '%s' указывает на директорию, а не на файл. Укажите имя файла.",
+                        path.toAbsolutePath()
+                ));
+            }
+        } else {
+            System.out.println(String.format(
+                    "Файл '%s' не найден в директории '%s'.",
+                    key,
+                    cachingDir
+            ));
+        }
+        return result;
     }
 }
