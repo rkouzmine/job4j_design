dd254aeecf23056bdf587c99806d6e1f6eaa4b1c
rkouzmine <krrsea@gmail.com>
2025-07-01 23:50:37 +0300

    Принципы Kiss, Dry и Yagni

---
+++ src/main/java/ru/job4j/kiss/fool/Fool.java
@@ -0,0 +1,47 @@
+package ru.job4j.kiss.fool;
+
+import java.util.Scanner;
+
+public class Fool {
+
+    public static void main(String[] args) {
+        System.out.println("Игра FizzBuzz.");
+        var startAt = 1;
+        var input = new Scanner(System.in);
+        while (startAt < 100) {
+            if (startAt % 3 == 0 && startAt % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (startAt % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (startAt % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(startAt);
+            }
+            startAt++;
+            var answer = input.nextLine();
+            if (startAt % 3 == 0 && startAt % 5 == 0) {
+                if (!"FizzBuzz".equals(answer)) {
+                    System.out.println("Ошибка. Начинай снова.");
+                    startAt = 0;
+                }
+            } else if (startAt % 3 == 0) {
+                if (!"Fizz".equals(answer)) {
+                    System.out.println("Ошибка. Начинай снова.");
+                    startAt = 0;
+                }
+            } else if (startAt % 5 == 0) {
+                if (!"Buzz".equals(answer)) {
+                    System.out.println("Ошибка. Начинай снова.");
+                    startAt = 0;
+                }
+            } else {
+                if (!String.valueOf(startAt).equals(answer)) {
+                    System.out.println("Ошибка. Начинай снова.");
+                    startAt = 0;
+                }
+            }
+            startAt++;
+        }
+    }
+}
